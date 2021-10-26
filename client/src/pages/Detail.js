327f42760fb4ffd38c4e8e03a1ff7f5c02a4829a
Erik Stone <spologas@gmail.com>
2021-10-26 12:19:05 -0700

    found out  that React and react-redux cannot import on same line.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
