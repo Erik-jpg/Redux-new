b7387f695cef0b51e1e65149ab73560c2d11fcf3
Erik Stone <spologas@gmail.com>
2021-10-25 17:11:17 -0700

    added some npms that the project was missing.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI ||  'mongodb+srv://Erik:503907Pdxm3@cluster0.lqy7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
