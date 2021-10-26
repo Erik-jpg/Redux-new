77fc647585b763da8ca58c0d75bc8cf73cc8019b
Erik Stone <spologas@gmail.com>
2021-10-26 12:23:38 -0700

    fixing errors.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useEffect, useState } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
